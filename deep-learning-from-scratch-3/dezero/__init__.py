4579166f9628cf4f97ec48ba521e1c870b26d728
minyoung <126471047+ggomaeng514@users.noreply.github.com>
2023-08-02 16:49:42 +0900

    Add files via upload

---
+++ deep-learning-from-scratch-3/dezero/__init__.py
@@ -0,0 +1,24 @@
+is_simple_core = False
+
+if is_simple_core:
+    from dezero.core_simple import Variable
+    from dezero.core_simple import Function
+    from dezero.core_simple import using_config
+    from dezero.core_simple import no_grad
+    from dezero.core_simple import as_array
+    from dezero.core_simple import as_variable
+    from dezero.core_simple import setup_variable
+# from dezero import Variable
+# 이런식으로 호출가능
+
+else:
+    from dezero.core import Variable
+    from dezero.core import Function
+    from dezero.core import using_config
+    from dezero.core import no_grad
+    from dezero.core import as_array
+    from dezero.core import as_variable
+    from dezero.core import setup_variable
+
+
+setup_variable()
